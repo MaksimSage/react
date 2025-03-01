68902f65780a041c18131da668fb99da45b03113
Гибкин Максим Сергеевич (4112843) <mgibkin@vtb.ru>
2025-03-01 11:11:32 +0300

    Updates

--- src/shared/baseComponents/Wrapper.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Container = styled.div`
-    background-color: lightblue;
-    padding-inline: 12px;
-    margin-left: auto;
-    margin-right: auto;
-    max-width: 1200px;
-`;
